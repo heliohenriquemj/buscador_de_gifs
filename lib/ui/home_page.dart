b1c6000b72b804d54c3fed503938d34684fd02fa
Helio Henrique <heliohenriquemj@hotmail.com>
2021-11-29 19:47:47 -0400

    buscador de gifs

---
+++ lib/ui/home_page.dart
@@ -0,0 +1,43 @@
+import 'dart:convert';
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  final String? _search = null;
+
+  final int _offset = 0;
+
+  Future<Map> _getGifs() async {
+    http.Response response;
+
+    if (_search == null) {
+      response = await http.get(Uri.parse(
+          "https://api.giphy.com/v1/gifs/trending?api_key=3kkcgzUWh2tJushOE8Wec0VJx7kB3s7t&limit=20&rating=g"));
+    } else {
+      response = await http.get(Uri.parse(
+          "https://api.giphy.com/v1/gifs/search?api_key=3kkcgzUWh2tJushOE8Wec0VJx7kB3s7t&q=$_search&limit=20&offset=$_offset&rating=g&lang=en"));
+    }
+    return json.decode(response.body);
+  }
+
+  @override
+  void initState() {
+    super.initState();
+
+    _getGifs().then((map) {
+      print(map);
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
